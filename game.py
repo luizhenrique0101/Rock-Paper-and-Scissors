eb34e649ea5f79b06c504a1a45d7bb400534a228
Luiz Henrique <105760898+luizhenrique0101@users.noreply.github.com>
2022-11-02 15:41:45 -0300

    RPS v2

--- game.py
+++ game.py
@@ -3,14 +3,16 @@ from os import system,name
 from colorama import Fore
 from rich.table import Table
 from rich.console import Console
-from tkinter import *
+from sys import exit
+import pygame
+from pygame.locals import *
 
 
 hand = ['Pedra','Papel','Tesoura']
 
 
-winsPc   = winsPlayer   = 0
-trophyPc = trophyPlayer = 0
+winsPc     = trophyPc     = roundsPc = 0
+winsPlayer = trophyPlayer = roundsPlayer = 0
 
 
 def clear():
@@ -28,7 +30,7 @@ def display_combination():
     print()
  
 
-'''def PlayerPlay():
+def PlayerPlay():
     while True:
         display_combination()
         try:
@@ -44,16 +46,37 @@ def display_combination():
             continue
         else:
             break
-    return indexPlayer'''
+    return indexPlayer
 
 
 def PcPlay():
     return randint(0,2)
 
 
-def WinVerify(indexRobot, indexPlayer):
-    global winsPc, winsPlayer, trophyPc, trophyPlayer
+def CounterPoints():
+    global winsPc, winsPlayer,  trophyPc, trophyPlayer, roundsPc, roundsPlayer
 
+    if winsPc >= 8:
+        trophyPc += 1
+        winsPc = 0
+
+    elif winsPlayer >= 8:
+        trophyPlayer += 1
+        winsPlayer = 0
+    
+    elif trophyPc == 5:
+        print('O computador ganhou a Partida! Não foi dessa vez...')
+        trophyPc = trophyPlayer = winsPc = winsPlayer = 0
+        roundsPc += 1
+    
+    elif trophyPlayer == 5:
+        print('Parabeeeens! Voce ganhou a Partida!')
+        trophyPc = trophyPlayer = winsPc = winsPlayer = 0
+        roundsPlayer += 1
+
+
+def WinVerify(indexRobot, indexPlayer):
+    global winsPc, winsPlayer
     #jogadas do computador 
     if  indexRobot == 0 and indexPlayer == 2 or indexRobot == 1 and indexPlayer == 0 or indexRobot == 2 and indexPlayer == 1:
         print(f'{Fore.RED}O computador ganhou! +1pt{Fore.RESET}')
@@ -66,22 +89,19 @@ def WinVerify(indexRobot, indexPlayer):
     else:
         print(f'{Fore.YELLOW}Empate{Fore.RESET}')
     
-    if winsPc >= 10:
-        trophyPc += 1
-        winsPc = 0
-    elif winsPlayer >= 10:
-        trophyPlayer += 1
-        winsPlayer = 0
-    
+    CounterPoints()
 
-def TablePoint():
+
+def TablePoints():
     console = Console()
     table = Table()
     table.add_column('Pc Wins')
     table.add_column('Player Wins')
     table.add_column('Pc Trophy')
     table.add_column('Player Trophy')
-    table.add_row(str(winsPc), str(winsPlayer), str(trophyPc), str(trophyPlayer))
+    table.add_column('Rounds Pc Wins')
+    table.add_column('Rounds Player Wins')
+    table.add_row(str(winsPc), str(winsPlayer), str(trophyPc), str(trophyPlayer), str(roundsPc), str(roundsPlayer))
     console.print(table)
 
 
@@ -90,32 +110,17 @@ def display_screen(indexRobot, indexPlayer):
     print(f'O computador jogou: {Fore.LIGHTMAGENTA_EX}{hand[indexRobot]}{Fore.RESET}')
     print(f'O jogador    jogou: {Fore.LIGHTCYAN_EX}{hand[indexPlayer]}{Fore.RESET}')
     WinVerify(indexRobot, indexPlayer)
-    TablePoint()
+    TablePoints()
     print('-'*30)
 
 
 def GameMain():
     while True:
         pc     = PcPlay()
-        #player = PlayerPlay()
-        #display_screen(pc,player)
+        player = PlayerPlay()
+        display_screen(pc,player)
         system('pause')
         clear()
 
 
-
-window = Tk()
-window.geometry('500x300+365+160')
-window.resizable(True,True)
-#window.minsize(500, 300)
-#window.maxsize(720, 400)
-#window.state('icon')     
-
-Label(window, text='Escolha uma das opções abaixo: ').grid(column=0, row=0)
-
-rock     = Button(window, text='Rock', command=None, padx=10).grid(column=1, row=2)
-paper    = Button(window, text='Paper', command=None, padx=10).grid(column=2, row=2)
-scissors = Button(window, text='Scissors', command=None, padx=10).grid(column=3, row=2)
-
-window.mainloop()
-
+GameMain()
